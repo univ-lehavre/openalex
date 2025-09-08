3d4452822f6d3020797547b1811f0fee8a289436
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-09-08 18:41:21 +0200

    feat: simplifier la configuration en supprimant les fonctions inutilisées et en ajoutant des paramètres d'environnement

--- src/config.ts
+++ src/config.ts
@@ -2,20 +2,18 @@ import process from 'node:process';
 import { Config, Effect } from 'effect';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { prepare, who } from './prompt';
 import { ConfigError } from 'effect/ConfigError';
-import { CommandLineError, PromptError } from './errors';
+import { CommandLineError } from './errors';
 import { Args, Env } from './types';
-
-const openalex_api_base = 'https://api.openalex.org';
-const openalex_authors_api_url = new URL(`${openalex_api_base}/authors`);
 
 const getEnv = (): Effect.Effect<Env, ConfigError, never> =>
   Effect.gen(function* () {
     const user_agent = yield* Config.string('USER_AGENT');
     const rate_limit_stringified = yield* Config.string('RATE_LIMIT');
+    const openalex_api_url = yield* Config.string('OPENALEX_API_URL');
+    const per_page = yield* Config.number('PER_PAGE');
     const rate_limit = JSON.parse(rate_limit_stringified);
-    return { user_agent, rate_limit };
+    return { user_agent, rate_limit, per_page, openalex_api_url };
   });
 
 const cmd = (): Effect.Effect<Args, CommandLineError, never> =>
@@ -27,22 +25,4 @@ const cmd = (): Effect.Effect<Args, CommandLineError, never> =>
     catch: (cause: unknown) => new CommandLineError(`Error while parsing arguments`, { cause }),
   });
 
-const prompt = (): Effect.Effect<{ name: string }, PromptError, never> =>
-  Effect.gen(function* () {
-    prepare('OpenAlex');
-    const { name } = yield* who();
-    return { name };
-  });
-
-const parameters = (): Effect.Effect<{ name: string }, CommandLineError | PromptError, never> =>
-  Effect.gen(function* () {
-    const argv = yield* cmd();
-    if (argv.name) {
-      return { name: argv.name as string };
-    } else {
-      const { name } = yield* prompt();
-      return { name };
-    }
-  });
-
-export { getEnv, parameters, openalex_authors_api_url };
+export { getEnv, cmd };
