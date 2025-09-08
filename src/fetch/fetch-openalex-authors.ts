1e6ed17129f812417680166a13f6ad34164f57fe
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-09-08 18:42:27 +0200

    feat: refactor la recherche d'auteurs et ajouter la fonction retrieve_articles

--- src/fetch/fetch-openalex-authors.ts
+++ src/fetch/fetch-openalex-authors.ts
@@ -1,47 +1,60 @@
 import { Effect } from 'effect';
-import { openalex_authors_api_url } from '../config';
+import { getEnv } from '../config';
 
 import { fetchAPI } from './fetch-openalex';
 import { ConfigError } from 'effect/ConfigError';
 import { FetchError, StatusError } from '../errors';
-import { OpenalexResponse, OpenalexSearchAuthorResult, Query } from '../types';
-
-const update_total_pages = <T>(response: OpenalexResponse<T>): number => {
-  const total_pages = Math.ceil(response.meta.count / response.meta.per_page);
-  return total_pages;
-};
-
-const get_results = <T>(response: OpenalexResponse<T>): T[] => {
-  return response.results as T[];
-};
+import { OpenalexResponse, AuthorsSearchResult, Query, WorksResult } from '../types';
 
 /**
- * Recherche des auteurs dans l'API OpenAlex par display_name et display_name_alternatives. Le second prénom, s’il n’est pas spécifié, renvoie tous les résultats.
+ * Recherche des auteurs dans l'API OpenAlex par display_name et display_name_alternatives.
+ * Le second prénom, s’il n’est pas spécifié, renvoie tous les résultats.
  * @param search Le terme de recherche.
  * @param start_page La page de résultats à partir de laquelle commencer la recherche.
  * @returns Une réponse contenant les résultats de la recherche.
  */
 const searchAuthors = (
   search: string,
-  start_page: number = 1
+  start_page: number = 1,
 ): Effect.Effect<
-  OpenalexResponse<OpenalexSearchAuthorResult>,
+  OpenalexResponse<AuthorsSearchResult>,
   ConfigError | StatusError | FetchError,
   never
 > =>
   Effect.gen(function* () {
+    const { per_page, openalex_api_url } = yield* getEnv();
+    const url = new URL(`${openalex_api_url}/authors`);
     const params: Query = {
       search,
+      per_page,
     };
-    const response: OpenalexResponse<OpenalexSearchAuthorResult> =
-      yield* fetchAPI<OpenalexSearchAuthorResult>(
-        openalex_authors_api_url,
-        params,
-        update_total_pages,
-        get_results,
-        start_page
-      );
+    const response: OpenalexResponse<AuthorsSearchResult> = yield* fetchAPI<AuthorsSearchResult>(
+      url,
+      params,
+      start_page,
+    );
     return response;
   });
 
-export { searchAuthors };
+const retrieve_articles = (
+  authors_ids: string[],
+  institutions_ids: string[],
+  start_page: number = 1,
+): Effect.Effect<OpenalexResponse<WorksResult>, ConfigError | StatusError | FetchError, never> =>
+  Effect.gen(function* () {
+    const { per_page, openalex_api_url } = yield* getEnv();
+    const url = new URL(`${openalex_api_url}/works`);
+    const filter = `author.id:${authors_ids.join('|')},institutions.id:${institutions_ids.join('|')},type:article`;
+    const params: Query = {
+      filter,
+      per_page,
+    };
+    const response: OpenalexResponse<WorksResult> = yield* fetchAPI<WorksResult>(
+      url,
+      params,
+      start_page,
+    );
+    return response;
+  });
+
+export { searchAuthors, retrieve_articles };
